5b7c03b4350315f728b0e99b78cfe9635c0232ba
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-02-25 11:54:27 +0100

    Switching to versioneer for versioning.

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+def parse_requirements(path):
+    """Parse ``requirements.txt`` at ``path``."""
+    requirements = []
+    with open(path, "rt") as reqs_f:
+        for line in reqs_f:
+            line = line.strip()
+            if line.startswith("-r"):
+                fname = line.split()[1]
+                inner_path = os.path.join(os.path.dirname(path), fname)
+                requirements += parse_requirements(inner_path)
+            elif line != "" and not line.startswith("#"):
+                requirements.append(line)
+    return requirements
+
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+test_requirements = parse_requirements("requirements/test.txt")
+install_requirements = parse_requirements("requirements/base.txt")
+
+setup(
+    author="Manuel Holtgrewe, Oliver Stolpe",
+    author_email=("manuel.holtgrewe@bihealth.de, oliver.stolpe@bihealth.de"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="REST API for CADD-scripts",
+    install_requires=install_requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="cadd varfish",
+    name="cadd-rest-api",
+    packages=find_packages(),
+    url="https://github.com/bihealth/cadd-rest-api",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    zip_safe=False,
+)
